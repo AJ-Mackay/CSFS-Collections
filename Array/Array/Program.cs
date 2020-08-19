f6b9ccfcdc3709b35e6cd4f91a14e975527007c6
Ruiner85 <paul.m.mackay85@gmail.com>
2020-08-19 15:20:05 +0100

    Arrays

--- Array/Array/Program.cs
+++ Array/Array/Program.cs
@@ -6,21 +6,42 @@ namespace Array
     {
         static void Main(string[] args)
         {
-            var items = new Item[5];
-            Random r = new Random();
-            for (int i = 0; i < items.Length; i++)
-            {
-                items[i] = new Item(r.Next());
-            }
+            // var items = new Item[5];
+            // Random r = new Random();
+            // for (int i = 0; i < items.Length; i++)
+            // {
+            //    items[i] = new Item(r.Next());
+            // }
             
-            Console.WriteLine("Items: ");
-            foreach (Item item in items)
+            // Console.WriteLine("Items: ");
+            // foreach (Item item in items)
+            // {
+            //    Console.WriteLine("ItemID = {0}", item.ID);
+            // }
+
+            // Item myItem = items[1];
+            // Console.WriteLine("ID of second item = {0}", myItem.ID);
+
+            var Authors = new String[5];
+            Authors[0] = "Faulkner";
+            Authors[1] = "Proust";
+            Authors[2] = "Stephenson";
+            Authors[3] = "Gibson";
+            Authors[4] = "Melville";
+            
+            Console.WriteLine("Authors...");
+            foreach (string s in Authors)
             {
-                Console.WriteLine("ItemID = {0}", item.ID);
+                Console.WriteLine(s);
             }
 
-            Item myItem = items[1];
-            Console.WriteLine("ID of second item = {0}", myItem.ID);
+            System.Array.Sort(Authors);
+            
+            Console.WriteLine("Authors...");
+            foreach (string s in Authors)
+            {
+                Console.WriteLine(s);
+            }
         }
     }
 }
