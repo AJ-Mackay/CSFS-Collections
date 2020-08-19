1ee45fa21d1261384ad4049b37ae90f903dabbe6
Ruiner85 <paul.m.mackay85@gmail.com>
2020-08-19 15:44:24 +0100

    Queues

---
+++ QueueCollection/QueueCollection/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QueueCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myQueue = new Queue<int>();
+            myQueue.Enqueue(100);
+            int val = myQueue.Dequeue();
+            // Console.WriteLine("val: {0}", val);
+
+            for (int i = 0; i < 50; i++)
+            {
+                myQueue.Enqueue(i);
+            }
+
+            foreach (int i in myQueue)
+            {
+                Console.Write("{0}, ", i);
+            }
+
+            var whichValue = myQueue.Dequeue();
+            Console.WriteLine("\nDequeued value: {0}", whichValue);
+            whichValue = myQueue.Dequeue();
+            Console.WriteLine("\nDequeued value: {0}", whichValue);
+        }
+    }
+}
